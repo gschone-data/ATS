3ef62bc06e12eedc45abbc2fa14fe62e0c02e05b
gschone-data <gillesschone@gmail.com>
2024-10-20 22:47:08 +0200

    correction mineure cours yahoo

--- cours/cours_yahoo.R
+++ cours/cours_yahoo.R
@@ -3,15 +3,15 @@ date_fin <- Sys.Date()
 date_debut <- make_date(year(date_fin) - 30, 1, 1)
 # Liste des paires de devises (par exemple EUR/USD, USD/JPY, etc.)
 
-for (item in liste_instruments_yahoo){
+for (item in liste_yahoo){
   as.data.frame(
     getSymbols(item, src = "yahoo", from = date_debut, to = date_fin))
 }
-for(instrument in liste_instruments_yahoo){
+for(instrument in liste_yahoo){
   assign(instrument,as.data.frame(get(instrument)) |> rownames_to_column())
 }
 
-for(instrument in liste_instruments_yahoo){
+for(instrument in liste_yahoo){
   pattern=paste0("^",{{instrument}},".")
   assign(instrument,
          get(instrument) |> 
